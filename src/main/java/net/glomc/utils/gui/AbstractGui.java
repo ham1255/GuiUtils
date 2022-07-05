4f0313cb0b9558dd69af8ed107daaa708d07a04e
mohammed jasem alaajel <xrambad@gmail.com>
2022-07-06 00:03:59 +0400

    updated depends, removed depercated code, fix eqauls, add drag event cancelation

--- src/main/java/net/glomc/utils/gui/AbstractGui.java
+++ src/main/java/net/glomc/utils/gui/AbstractGui.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
@@ -18,7 +19,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
@@ -29,9 +32,11 @@ public abstract class AbstractGui implements CommandExecutor, Listener {
     protected final Plugin plugin;
     protected boolean unRegisterListenerWhenClosed;
 
+    private boolean enableDragging = false;
+
     public enum Rows {
         ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6);
-        int rows;
+        final int rows;
 
         Rows(int rows) {
             this.rows = rows * 9;
@@ -48,7 +53,7 @@ public abstract class AbstractGui implements CommandExecutor, Listener {
     protected AbstractGui(String name, Rows rows, Boolean unRegisterListenerWhenClosed, Plugin plugin) {
         this.rows = rows.getRows();
         this.name = translateAlternateColorCodes('&', name);
-        inventory = Bukkit.createInventory(null, this.rows, this.name);
+        this.inventory = Bukkit.createInventory(null, this.rows, this.name);
         this.unRegisterListenerWhenClosed = unRegisterListenerWhenClosed;
         this.plugin = plugin;
     }
@@ -60,63 +65,30 @@ public abstract class AbstractGui implements CommandExecutor, Listener {
 
 
     protected void fillGUI(Material material, Integer... excluded) {
-        int slot = -1;
-        ItemStack item = new ItemStack(material, 1);
-        ItemMeta item_meta = item.getItemMeta();
-        assert item_meta != null;
-        item_meta.setDisplayName(translateAlternateColorCodes('&', "&1."));
-        item.setItemMeta(item_meta);
-        item.setAmount(1);
-        try {
-            while (slot <= (this.rows - 2)) {
-                slot++;
-                int finalSlot = slot;
-                if (excluded != null && Arrays.stream(excluded).noneMatch(i -> i == finalSlot)) {
-                    this.inventory.setItem(slot, item);
-                }
+        if (excluded == null) {
+            excluded = new Integer[]{};
+        }
+        List<Integer> excludedSlots = Arrays.asList(excluded);
+        ItemStack itemStack = new ItemStack(material, 1);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        assert itemMeta != null;
+        itemMeta.setDisplayName(translateAlternateColorCodes('&', "&1."));
+        itemStack.setItemMeta(itemMeta);
+        itemStack.setAmount(1);
+        for (int i = 0; i < rows; i++) {
+            if (excludedSlots.contains(i)) {
+                continue;
             }
-        } catch (Exception ignored) {
-
+            inventory.setItem(i, itemStack);
         }
 
 
     }
 
-
-    @Deprecated
-    protected void enchantItem(int slot) {
-        ItemStack itemStack = inventory.getItem(slot);
-        if (itemStack == null) {
-            return;
-        }
-        ItemMeta meta = itemStack.getItemMeta();
-        if (meta == null) {
-            return;
-        }
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        meta.addEnchant(Enchantment.FIRE_ASPECT, 1, false);
-        itemStack.setItemMeta(meta);
-        inventory.setItem(slot, itemStack);
-    }
-
-    @Deprecated
-    protected void removeEnchantFromItem(int slot) {
-        ItemStack itemStack = inventory.getItem(slot);
-        if (itemStack == null) {
-            return;
-        }
-        ItemMeta meta = itemStack.getItemMeta();
-        if (meta == null) {
-            return;
-        }
-        meta.removeEnchant(Enchantment.FIRE_ASPECT);
-        itemStack.setItemMeta(meta);
-        inventory.setItem(slot, itemStack);
-    }
 
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
-        if (isEqual(event.getView())) {
+        if (event.getClickedInventory() == this.inventory) {
             event.setCancelled(true);
             onClick(event);
         }
@@ -124,19 +96,22 @@ public abstract class AbstractGui implements CommandExecutor, Listener {
     }
 
     @EventHandler
-    public void onInventoryCloseEvent(InventoryCloseEvent event) {
-        if (isEqual(event.getView())) {
-            if (unRegisterListenerWhenClosed) {
-                InventoryClickEvent.getHandlerList().unregister(this);
-                InventoryCloseEvent.getHandlerList().unregister(this);
-            }
-
+    public void onInventoryDragEvent(InventoryDragEvent event) {
+        if (event.getInventory() == this.inventory) {
+            event.setCancelled(!this.enableDragging);
         }
     }
 
+    @EventHandler
+    public void onInventoryCloseEvent(InventoryCloseEvent event) {
+        if (event.getInventory() == this.inventory) {
+            if (unRegisterListenerWhenClosed) {
+                InventoryClickEvent.getHandlerList().unregister(this);
+                InventoryCloseEvent.getHandlerList().unregister(this);
+                InventoryDragEvent.getHandlerList().unregister(this);
+            }
 
-    private boolean isEqual(InventoryView view) {
-        return view.getTopInventory().equals(this.inventory) || view.getTitle().equals(ChatColor.translateAlternateColorCodes('&', name));
+        }
     }
 
     public abstract void onClick(InventoryClickEvent event);
@@ -163,4 +138,7 @@ public abstract class AbstractGui implements CommandExecutor, Listener {
         player.openInventory(this.inventory);
     }
 
+    public void enableDragging(boolean enableDragging) {
+        this.enableDragging = enableDragging;
+    }
 }
